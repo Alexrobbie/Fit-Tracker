f42d36860b167930c56b0c0f7745524813dea15a
Alexrobbie <robinsonmail25@gmail.com>
2020-05-24 14:44:54 -0500

    Upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Fit-Tracker
+
+A fittness tracking app that keeps track of user entered data
---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+var path = require("path");
+
+const mongoose = require("mongoose");
+const db = require("./models/workout");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", { useNewUrlParser: true });
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.get("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+app.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname + "/public/stats.html"));
+});
+
+app.get("/api/workouts", function(req, res) {
+    db.find({}, (err, results) => {
+        res.json(results);
+    });
+});
+
+app.get("/api/workouts/range", function(req, res) {
+    db.find({}, (err, results) => {res.json(results)});
+});
+
+app.post("/api/workouts", function(req, res) {
+    db.collection.insertOne({day: Date.now(), exercises: []})
+    .then(data => {res.json(data)})
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
+});
+
+app.put("/api/workouts/:id", function(req, res) {
+    console.log(req.body);
+    db.findOne({_id: req.params.id}, (err, results) => {console.log(results)})
+    db.updateOne({_id: req.params.id}, { $push: {exercises: req.body} }, (err, result) => {res.json(result)});
+});
+
+app.listen(PORT, function() {
+    console.log("App running at localhost:" + PORT);
+});
